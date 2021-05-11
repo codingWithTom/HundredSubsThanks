9f7b9144fa12b5a2b7cb3c4984b2db4f50d7bdb4
Tomas Trujillo <t.trujillo138@gmail.com>
2021-05-10 19:39:40 -0700

    Initial Commit

---
+++ HundredSubsThanks/HundredSubsThanksApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredSubsThanksApp.swift
+//  HundredSubsThanks
+//
+//  Created by Tomas Trujillo on 2021-05-10.
+//
+
+import SwiftUI
+
+@main
+struct HundredSubsThanksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
